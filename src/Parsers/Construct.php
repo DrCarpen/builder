c2bc3100ec4c807ae8c17e81d476676479d7c341
nj_liyang <liyang@uniondrug.cn>
2019-05-08 11:56:03 +0800

    x0.2 float 类型

--- src/Parsers/Construct.php
+++ src/Parsers/Construct.php
@@ -78,7 +78,7 @@ class Construct
                 return 'string';
                 break;
             case in_array($type, $this->float):
-                return 'double';
+                return 'float';
                 break;
             default:
                 return 'string';
