3ef31cae187378325567306ae5204b32fdeab832
william-wqq <william_wqq@163.com>
2020-07-13 18:50:57 +0800

    添加数据库选择

--- src/Commands/Builder.php
+++ src/Commands/Builder.php
@@ -5,6 +5,8 @@
  */
 namespace Uniondrug\Builder\Commands;
 
+use Phalcon\Config;
+use Symfony\Component\Console\Exception\RuntimeException;
 use Symfony\Component\Console\Input\InputAwareInterface;
 use Uniondrug\Builder\Modes\SimpleMode;
 use Uniondrug\Builder\Modes\SingleApiMode;
@@ -18,10 +20,13 @@ use Uniondrug\Builder\Tools\Console;
  */
 class Builder extends Command
 {
+    /**
+     * @var string
+     */
     protected $signature = 'builder
-                            {--table= : 数据表名}
-                            {--api= : 接口名称，支持自定义接口}
-                            ';
+            {--table=|-t : Table\'s name}
+            {--database=|-d : Database\'s name [databases.example_db]}
+            {--api= : 接口名称，支持自定义接口}';
     /**
      * 命令描述
      * @var string
@@ -56,12 +61,72 @@ class Builder extends Command
     }
 
     /**
+     * 获取默认的数据库连接
+     * @return mixed|Config
+     */
+    public function getDefaultDatabase()
+    {
+        return \app()->getConfig()->database->connection;
+    }
+
+    /**
+     * 解析输入数据库参数
+     * @return Config
+     */
+    public function parseDatabase()
+    {
+        // 参数databases.example_db
+        $inputDbStr = $this->input->getOption('database');
+        if ($inputDbStr === null) {
+            return;
+        }
+        if (empty($inputDbStr)) {
+            throw new RuntimeException('The option of --database value is required');
+        }
+        $inputDbArr = explode('.', $inputDbStr);
+        $filePath = \app()->configPath().'/'.current($inputDbArr).'.php';
+        // 格式不正确
+        if (count($inputDbArr) != 2) {
+            throw new RuntimeException('The option of --database format must be databases.example_db');
+        }
+        // 配置文件不存在
+        if (!file_exists($filePath)) {
+            throw new RuntimeException('The file of '.current($inputDbArr).' not exist !');
+        }
+        // 配置文件的数据库配置不存在
+        $dbConfig = \config()->path($inputDbStr);
+        if (!$dbConfig) {
+            throw new RuntimeException('The config '.next($inputDbArr).' not exist!');
+        }
+        return $dbConfig;
+    }
+
+    /**
+     * 解析输入表参数
+     */
+    public function parseTable()
+    {
+        $inputTable = $this->input->getOption('table');
+        if (!isset($inputTable)) {
+            throw new RuntimeException('The option of --table is required');
+        }
+        if (empty($inputTable)) {
+            throw new RuntimeException('The option of --table value is required');
+        }
+    }
+
+    /**
      * 检查数据库配置
      * @return bool
      */
     private function _checkDatabase()
     {
-        $connection = app()->getConfig()->database->connection;
+        // 解析输入数据库参数
+        $dbConfig = $this->parseDatabase();
+        // 解析输入表参数
+        $table = $this->parseTable();
+        $connection = $dbConfig ?: $this->getDefaultDatabase();
+        // 检查数据库链接是否存在
         if (empty(app()->getConfig()->database)) {
             $this->console->errorExit('目录文件/config/database.php 不存在，请检查目录');
         }
